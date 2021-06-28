e24d11e648b2644e780cfcb9fa7c296bd2515109
Asfffffd <84439113+Asfffffd@users.noreply.github.com>
2021-06-28 10:27:09 +0200

    Update and rename installDejian2021.sh to installDejian2021_rK.sh

--- installDejian2021_rK.sh
+++ installDejian2021_rK.sh
@@ -6,7 +6,7 @@ git clone https://gitlab.dune-project.org/core/dune-istl.git
 git clone https://gitlab.dune-project.org/extensions/dune-foamgrid.git
 git clone https://gitlab.dune-project.org/extensions/dune-alugrid.git
 git clone https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
-git clone https://github.com/twophaseDFM/DFM2PTH.git
+git clone https://github.com/twophaseDFM/RandomK_3D.git
 ./dune-common/bin/dunecontrol --opts=dumux/cmake.opts all
 
 
